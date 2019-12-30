25d4ffe5a67b8dfaa3a8e03330224682006d2de8
Ken Biondi <kenbiondi@hotmail.com>
2019-12-30 10:35:13 -0800

    exercise 41

--- Exercise39.rkt
+++ Exercise39.rkt
@@ -1,9 +1,17 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise39) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-(define WIDTH-OF-WORLD 200)
-(define WHEEL-RADIUS 5)
+(define WIDTH-OF-WORLD 1000)
+(define WHEEL-RADIUS 9)
 (define WHEEL-DISTANCE (* WHEEL-RADIUS 5))
+(define Y-CAR (* WHEEL-RADIUS 3))
+(define TREE
+  (underlay/xy (circle WHEEL-RADIUS "solid" "green")
+                WHEEL-RADIUS (* WHEEL-RADIUS 1.5)
+               (rectangle 2 (* WHEEL-RADIUS 2) "solid" "brown")))
+
+(define BACKGROUND (overlay/align "middle" "bottom" TREE
+                    (rectangle WIDTH-OF-WORLD (* WHEEL-RADIUS 6) "solid" "black")))
 
 (define WHEEL
   (circle WHEEL-RADIUS "solid" "white"))
@@ -15,31 +23,35 @@
   (beside WHEEL SPACE WHEEL))
 
 (define CAR-TOP
-  (rectangle 20 7 "solid" "red"))
+  (rectangle (* WHEEL-RADIUS 5) (* WHEEL-RADIUS 1.5) "solid" "red"))
 
 (define CAR-BOTTOM
-  (rectangle 55 10 "solid" "red"))
+  (rectangle (* WHEEL-RADIUS 11) (* WHEEL-RADIUS 2) "solid" "red"))
 
 (define CAR-BODY
   (above CAR-TOP CAR-BOTTOM))
 
 (define CAR
-  (overlay/align/offset "middle" "bottom" BOTH-WHEELS 0 -5 CAR-BODY))
+  (overlay/align/offset "middle" "bottom" BOTH-WHEELS 0 (- WHEEL-RADIUS) CAR-BODY))
+
 
 ; A WorldState is a Number.
 ; interpretation the number of pixels between
 ; the left border of the scene and the car
 
 ; WorldState -> Image
-; places the image of the car x pixels from 
-; the left margin of the BACKGROUND image 
-;(define (render x)
-;  BACKGROUND)
+; places the car into the BACKGROUND scene,
+; according to the given world state 
+ (define (render ws)
+   (place-image CAR (- ws (/ (image-width CAR) 2)) Y-CAR BACKGROUND))
  
 ; WorldState -> WorldState
-; adds 3 to x to move the car right 
-(define (tock x)
-  x)
+; adds 3 to world state to move the car right for every clock tick
+(check-expect (tock 20) 23)
+(check-expect (tock 78) 81)
+(define (tock ws)
+  (+ ws 3))
+   
  
 ; WorldState String -> WorldState 
 ; for each keystroke, big-bang obtains the next state 
@@ -54,11 +66,14 @@
  
 ; WorldState -> Boolean
 ; after each event, big-bang evaluates (end? cw) 
-(define (end? cw) ...)
+(define (end? ws) (cond
+                    [(< ws (+ (image-width BACKGROUND) (image-width CAR))) #false]
+                    [(>= ws (+ (image-width BACKGROUND) (image-width CAR))) #true]))
 
 ; WorldState -> WorldState
 ; launches the program from some initial state 
-;(define (main ws)
-;   (big-bang ws
-;     [on-tick tock]
-;     [to-draw render]))
+(define (main ws)
+   (big-bang ws
+     [on-tick tock]
+     [to-draw render]
+     [stop-when end?]))
